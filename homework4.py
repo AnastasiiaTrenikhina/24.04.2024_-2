ee3e4c71e2385a315d79ae255946e333c65045b8
AnastasiiaTrenikhina <167620693+AnastasiiaTrenikhina@users.noreply.github.com>
2024-04-24 09:29:50 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+#immutable_var= 8,9,'cat','dog'
+#print(immutable_var)
+#immutable_var[0]=21 #  в кортеже нельзя заменять/изменять. Замена возможна только в списках
+
+
+mutable_list=["Banan",'Cherry","London',"33","89"]
+print(mutable_list)
+mutable_list[2]="Italy"
+print(mutable_list)
+
